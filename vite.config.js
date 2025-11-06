6df210ca9c976da8b4fc61151a175b99515f837c
victor-pcll <victor.peucelle@epfl.ch>
2025-11-06 21:03:01 +0100

    Deployement tests

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/Website/', // / pour dev, /Website/ pour GitHub Pages
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}))
+  base: '/Website/', // pour GitHub Pages
+})
